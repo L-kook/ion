c274c6f279a14ff3dbd75cd663bc067ee0abcbdb
David Alsh <alshdavid@gmail.com>
2025-09-07 16:12:15 +1000

    rename file

--- crates/ion/src/utils/mod.rs
+++ crates/ion/src/utils/mod.rs
@@ -1,6 +1,6 @@
 pub mod channel;
+pub mod debug;
 pub mod hash_map_ext;
-pub mod id;
 mod os_string_ext;
 mod path_ext;
 pub mod ref_counter;
@@ -9,6 +9,6 @@ pub mod v8;
 
 pub use self::os_string_ext::*;
 pub use self::path_ext::*;
+pub use debug::*;
 pub use hash_map_ext::*;
-pub use id::*;
 pub use ref_counter::*;
