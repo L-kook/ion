bfdf1857cad21f908a22edf9ceeaa9cf15b48d51
David Alsh <alshdavid@gmail.com>
2025-09-15 16:27:18 +1000

    ok

--- crates/ion/src/utils/hash.rs
+++ crates/ion/src/utils/hash.rs
@@ -4,5 +4,5 @@ use sha2::Sha256;
 
 pub fn hash_sha256(bytes: &[u8]) -> String {
     let result = Sha256::digest(bytes);
-    format!("{}", base64::prelude::BASE64_STANDARD.encode(result))
+    base64::prelude::BASE64_STANDARD.encode(result).to_string()
 }
